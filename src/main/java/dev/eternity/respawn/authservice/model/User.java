ebaef8cbeee6aeaf836f6d1a974bdea5ab2d7656
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2025-02-14 21:14:16 +0200

    Set up liquibase and added changesets for users and oauth_tokens tables

--- src/main/java/dev/eternity/respawn/authservice/model/User.java
+++ src/main/java/dev/eternity/respawn/authservice/model/User.java
@@ -2,24 +2,25 @@ package dev.eternity.respawn.authservice.model;
 
 import java.util.HashSet;
 import java.util.Set;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Table(name = "users")
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(nullable = false, unique = true)
     private String email;
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private Set<OauthToken> tokens = new HashSet<>();
 }
