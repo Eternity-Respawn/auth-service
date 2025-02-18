f076d3faf4190b8eae36a4ab9b39b8a44e26cef0
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2025-02-18 15:29:50 +0200

    Fixed SecurityConfig

--- src/main/java/dev/eternity/respawn/authservice/config/SecurityConfig.java
+++ src/main/java/dev/eternity/respawn/authservice/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package dev.eternity.respawn.authservice.config;
 
+import dev.eternity.respawn.authservice.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +11,19 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final UserDetailsService userDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Value("${public.endpoints}")
     private String[] publicEndpoints;
 
@@ -43,6 +51,11 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                )
+                .userDetailsService(userDetailsService)
                 .build();
     }
 }
